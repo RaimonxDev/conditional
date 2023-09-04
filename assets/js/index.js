54c26807729da7ef49e3369842d185d547104444
Ramon Martinez Work <ramonmartinezcordero@gmail.com>
2023-09-04 19:31:14 -0300

    complete

--- assets/js/index.js
+++ assets/js/index.js
@@ -8,7 +8,16 @@ const inputStickerGit = document?.querySelector('.input--sticker_git');
 const btnVerified = document?.querySelector('.verifiedStickers');
 const messageStickers = document?.querySelector('.message__stickers');
 const limitStickers = 10;
-// Ejemplo 1
+
+// Ejercicio 3 
+
+const btnVerifiedPass = document?.querySelector('.verifiedPassword');
+const fieldOne = document?.querySelector('#fieldOne');
+const fieldTwo = document?.querySelector('#fieldTwo');
+const fieldThird = document?.querySelector('#fieldThird');
+const messagePassword = document?.querySelector('.messagePass');
+
+
 document?.addEventListener('DOMContentLoaded', () => { 
   // ejecicio 1 
   exampleImage?.addEventListener('click', () => { 
@@ -27,25 +36,27 @@ document?.addEventListener('DOMContentLoaded', () => {
     event.preventDefault();
     const stickersTotal = parseInt(inputStickerJs.value) + parseInt(inputStickerCss.  value) + parseInt(inputStickerGit.value);
     if (stickersTotal === 0) { 
-      messageStickers.innerHTML = 'No llevas stickers';
-      return
+      return messageStickers.innerHTML = 'No llevas stickers';
     }
     if (stickersTotal > limitStickers) {
-      messageStickers.innerHTML = 'Llevas demaiados stickers';
-      return
+      return messageStickers.innerHTML = 'Llevas demaiados stickers';
     }
-
     messageStickers.innerHTML = `Llevas ${stickersTotal} stickers` ;  
   });
 
   // ejercicio 3
 
-
-
-
-
-
-
+  btnVerifiedPass?.addEventListener('click', (event) => {
+    event.preventDefault();
+    const password = fieldOne.value + fieldTwo.value + fieldThird.value;
+    if (password === "714") {
+      return messagePassword.innerHTML = 'Contraseña 2 correcta';
+    }
+    if (password === "911") {
+      return messagePassword.innerHTML = 'Contraseña correcta';
+    }
+    return messagePassword.innerHTML = 'Contraseña incorrecta';
+  });
 
 });
 
